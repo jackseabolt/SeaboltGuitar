d9a798d88c063cd4c24953c5717b9e8ffc5b35fc
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-04-05 15:15:30 -0700

    controller tests added

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,23 +2,22 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
 
+let!(:first_post) { Post.create(title: "Title", body: "Body")}
+
 	describe "GET index" do 
 
+		before { get :index }
+
 		it 'returns with http success' do 
-			get :index
 			expect(response).to have_http_status(:success)
 		end 
 
 		it 'renders index view' do 
-			get :index
 			expect(response).to render_template('index')
 		end
 
-		# Does not include multiple posts in the example
-		it 'assigns @posts' do 
-			all_posts = Post.create(title: "title", body: "test")
-			get :index 
-			expect(assigns(:posts)).to eq([all_posts])
+		it 'assigns @posts' do  
+			expect(assigns(:posts)).to eq([first_post])
 		end 
 
 	end 
@@ -27,8 +26,7 @@ RSpec.describe PostsController, type: :controller do
 	describe "GET show" do 
 
 		before do 
-			@post = Post.create(title: 'test', body: 'testing')
-			get :show, params: { id: @post.id } 
+			get :show, params: { id: first_post.id } 
 		end
 
 		it "returns with http success" do 
@@ -39,19 +37,21 @@ RSpec.describe PostsController, type: :controller do
 			expect(response).to render_template('show')
 		end
 
-		# this test does not work
-		# it "assigns @post" do 
-		# 	expect(assigns(:post)).to eq([@post])
-		# end
+		
+		it "assigns @post" do 
+			expect(assigns(:post)).to eq(first_post)
+		end
 
 
 	end
 
 	describe "GET new" do 
 
-		before do 
-			get :new
-		end
+		before { get :new }
+
+		it "assigns @post" do 
+			expect(assigns(:post)).to be_a_new(Post)
+		end 
 
 		it "returns with http success" do 
 			expect(response).to have_http_status(:success)
@@ -66,18 +66,61 @@ RSpec.describe PostsController, type: :controller do
 
 	describe "POST create" do  
 
-		# "param is missing or the value is empty: post"
-		# it "responds with http success" do 
-		# 	post :create, params: { title: "test", body: "test" }
-		# 	expect(response).to have_http_status(:success)
-		# end 
+		context "when valid" do 
+			before { post :create, post: {title: "title", body: "body"}}
 
-		# it "redirects to the index view" do 
-		# 	post :create, params: { title: "test", body: "test" }
-		# 	expect(response).to redirect_to posts_path
-		# end 
+			it "will redirect to root path" do 
+				expect(response).to redirect_to(posts_path)
+			end 
 
+			it "will flash[:success]" do 
+				expect(flash[:success]).to be_present
+			end 
+		end 
+
+		context "when invalid" do 
+			before { post :create, post: {title: "title"} }
+
+			it "will render the new template" do 
+				expect(response).to render_template('new')
+			end 
+		end 
 	end 
+
+	describe "PUT update" do 
+
+		context "when valid" do 
+			before { put :update, post: { title:"title", body: "body" }, id: first_post.id }
+
+			it "will redirect to posts path" do 
+				expect(response).to redirect_to(posts_path)
+			end
+
+			it "will flash[:success]" do 
+				expect(flash[:success]).to be_present
+			end
+		end 
+
+		context "when invalid" do 
+			before { put :update, post: {title: "testing",}, id: first_post.id }
+
+			it "will render the edit post path" do 
+				expect(response).to render_template('edit')
+			end 
+		end 
+	end 
+
+	describe "DELETE destroy" do 
+		before { delete :destroy, id: first_post.id }
+
+		it "will redirect to posts path" do 
+			expect(response).to redirect_to(posts_path)
+		end
+
+		it "will flash[:success]" do 
+			expect(flash[:success]).to be_present
+		end 
+	end
 
 end
 
