ec1acd5e17d08149f4af3a3e4bad26faf3a35115
John Seabolt <johnseabolt@Johns-MacBook-Pro.local>
2017-04-11 08:39:27 -0700

    pagination added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
 	def index 
-		@posts = Post.all
+		@posts = Post.all.order(created_at: :desc).paginate(:page => params[:page], :per_page => 4)
 	end 
 
 	def show 
